d67abcd1d11b642e1497af040228ac50016fba12
Diogo Freire <diogofre@gmail.com>
2024-05-22 15:39:39 -0300

    [FEAT] Add AbstractRepository

--- app/Repository/ModeloRepository.php
+++ app/Repository/ModeloRepository.php
@@ -8,51 +8,16 @@
 
 namespace App\Repository;
 
-use Illuminate\Database\Eloquent\Model;
-
-class ModeloRepository
+/**
+ * Class ModeloRepository.
+ *
+ * @codingStandardsIgnoreStart
+ *
+ * @method string selectWithAtributtes(string $atributos)
+ *
+ * @codingStandardsIgnoreEnd
+ */
+class ModeloRepository extends AbstractRepository
 {
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
 
-    /**
-     * @param $atributos
-     * @return void
-     */
-    public function selectWithAtributtes($atributos) {
-        $this->model = $this->model->with($atributos);
-        //a query está sendo montada
-    }
-
-    /**
-     * @param $filtros
-     * @return void
-     */
-    public function filtro($filtros) {
-        $filtros = explode(';', $filtros);
-
-        foreach($filtros as $key => $condicao) {
-
-            $c = explode(':', $condicao);
-            $this->model = $this->model->where($c[0], $c[1], $c[2]);
-            //a query está sendo montada
-        }
-    }
-
-    /**
-     * @param $atributos
-     * @return void
-     */
-    public function selectRaw($atributos) {
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getResultado() {
-        return $this->model->get();
-    }
 }
