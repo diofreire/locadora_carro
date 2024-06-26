f078da78950559a6be49071083dbf107ebfbec66
Diogo Freire <diogofre@gmail.com>
2024-06-26 17:37:48 -0300

    Abstração do componente table

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -2,16 +2,21 @@
     <div>
         <table class="table table-hover">
             <thead>
-            <tr >
-                <th v-for="t, key in titulos" :key="key" scope="col">{{ t }}</th>
-            </tr>
+                <tr >
+                    <th v-for="t, key in titulos" :key="key" scope="col" class="text-uppercase">{{ t }}</th>
+                </tr>
             </thead>
             <tbody>
-            <tr v-for="m in dados" :key="m.id">
-                <th scope="row">{{m.id}}</th>
-                <td>{{m.nome}}</td>
-                <td><img :src="'/storage/'+m.imagem" width="30" height="30"></td>
-            </tr>
+                <tr v-for="obj in dados" :key="obj.id">
+                    <td v-if="titulos.includes(chave)" v-for="valor, chave in obj" :key="chave">
+                        <span v-if="chave == 'imagem'">
+                            <img :src="'/storage/'+valor" width="30" height="30">
+                        </span>
+                        <span v-else>
+                           {{ valor }}
+                        </span>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
